9404d72af9f34abe0e38eda9f5c341f6d01bb9cc
MD Mostafa Niloy <mostafa@visernic.com>
2025-12-01 22:02:34 +0600

    Create assets.js

---
+++ src/config/assets.js
@@ -0,0 +1,16 @@
+/**
+ * Asset Manager
+ * Stores remote URLs for game resources.
+ */
+
+export const ASSETS = {
+    images: {
+        scythe: "https://img.icons8.com/?size=100&id=6IyXfrxdkMa5&format=png&color=FFFFFF",
+        coin: "https://img.icons8.com/?size=100&id=A05obJXqB1TJ&format=png&color=000000",
+        defaultAvatar: "https://ui-avatars.com/api/?background=333&color=fff"
+    },
+    icons: {
+        ninja: "fa-solid fa-user-ninja",
+        astronaut: "fa-solid fa-user-astronaut"
+    }
+};
