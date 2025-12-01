7edcd3fcd8e48e8295c8c49b795051e0efa8df37
MD Mostafa Niloy <mostafa@visernic.com>
2025-12-01 22:11:22 +0600

    Create Vector2D.js

---
+++ src/core/physics/Vector2D.js
@@ -0,0 +1,33 @@
+/**
+ * Vector Math Utility
+ * Professional math class for physics calculations.
+ */
+
+export class Vector2D {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    add(v) {
+        return new Vector2D(this.x + v.x, this.y + v.y);
+    }
+
+    subtract(v) {
+        return new Vector2D(this.x - v.x, this.y - v.y);
+    }
+
+    multiply(scalar) {
+        return new Vector2D(this.x * scalar, this.y * scalar);
+    }
+
+    mag() {
+        return Math.sqrt(this.x * this.x + this.y * this.y);
+    }
+
+    static distance(v1, v2) {
+        const dx = v1.x - v2.x;
+        const dy = v1.y - v2.y;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+}
