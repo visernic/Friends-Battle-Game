afbae8dbc2f56bfe7991b0d380c9368548ecc7da
MD Mostafa Niloy <mostafa@visernic.com>
2025-12-02 12:57:06 +0600

    Create bootstrap.php

---
+++ Public/internal/system/core/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+
+spl_autoload_register(function($class) {
+    $path = ROOT . '/' . str_replace('\\', '/', $class) . '.php';
+    if (file_exists($path)) {
+        require $path;
+    }
+});
