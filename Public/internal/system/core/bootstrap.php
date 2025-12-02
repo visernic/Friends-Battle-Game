65be7158689a81f1b47e372c937b7f23315dad0c
Sarfenaz Naz <sarfenaz.naz@gmail.com>
2025-12-02 12:56:22 +0600

    Delete Public/internal/system/core directory

--- Public/internal/system/core/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-spl_autoload_register(function($class) {
-    $path = ROOT . '/' . str_replace('\\', '/', $class) . '.php';
-    if (file_exists($path)) {
-        require $path;
-    }
-});
